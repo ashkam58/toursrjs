ba0bee7a884d06e8d9633fdd553905a84d11b29d
ashkam58 <ashkam58@gmail.com>
2024-02-29 09:35:41 +0530

    made a refresh button,

--- tour/src/Tour.js
+++ tour/src/Tour.js
@@ -1,12 +1,25 @@
 import React, {useState} from 'react'
 
 const Tour = ({
-id, image, name
+id, image, name,price, info, removeTour
 }) => {
+  const [readMore, setReadMore] = useState(false)
+
+
   return (
-    <article>
+    <article className='single-tour'>
       <img src={image} alt="" />
+      <footer>
+      <div className="tour-info">
       <h4>{name}</h4>
+      <h4 className="tour-price">${price}</h4>
+      </div>
+      <p>{readMore ? info : `${info.substring(0,200)}`}....
+      <button onClick={()=>setReadMore(!readMore)}>{readMore ? 'show less' : 'Read more'}</button></p>
+      <button className="delete-btn" onClick={()=>removeTour(id)}>
+        not-interested
+      </button>
+      </footer>
     </article>
   )
 }
